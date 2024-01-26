c7f32d630ed500ad87e7a790b9fa465f6580f343
Buzgau Catalin <buzgaucatalin@yahoo.com>
2024-01-26 17:58:35 +0200

    Update user

--- backend/src/main/java/dev/cata/backend/customer/CustomerService.java
+++ backend/src/main/java/dev/cata/backend/customer/CustomerService.java
@@ -1,6 +1,7 @@
 package dev.cata.backend.customer;
 
 import dev.cata.backend.exception.DuplicateResourceException;
+import dev.cata.backend.exception.RequestValidationException;
 import dev.cata.backend.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -45,5 +46,34 @@ public class CustomerService {
                     "customer with id [%s] not found".formatted(customerId)
             );
         customerDao.deleteCustomerById(customerId);
+    }
+
+    public void updateCustomer(Integer customerId, CustomerUpdateRequest updateRequest) {
+        Customer customer = getCustomer(customerId);
+
+        boolean changes = false;
+        if (updateRequest.name() != null && !updateRequest.name().equals(customer.getName())) {
+            customer.setName(updateRequest.name());
+            changes = true;
+        }
+
+        if (updateRequest.age() != null && !updateRequest.age().equals(customer.getAge())) {
+            customer.setAge(updateRequest.age());
+            changes = true;
+        }
+
+        if (updateRequest.email() != null && !updateRequest.email().equals(customer.getEmail())) {
+            if (customerDao.existsPersonWithEmail(updateRequest.email()))
+                throw new DuplicateResourceException(
+                        "email already taken"
+                );
+            customer.setEmail(updateRequest.email());
+            changes = true;
+        }
+
+        if (!changes)
+            throw new RequestValidationException("no data changes found");
+
+        customerDao.updateCustomer(customer);
     }
 }
---
+++ backend/src/main/java/dev/cata/backend/customer/CustomerUpdateRequest.java
@@ -0,0 +1,8 @@
+package dev.cata.backend.customer;
+
+public record CustomerUpdateRequest(
+        String name,
+        String email,
+        Integer age
+) {
+}
